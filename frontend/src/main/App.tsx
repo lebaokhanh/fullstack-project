4b9a066c1cdb0e2b5762853cc3715eaacabd93d9
Khanh Le <lebaokhanh1991@gmail.com>
2021-11-29 23:32:03 -0700

    - add router

--- frontend/src/main/App.tsx
+++ frontend/src/main/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route, useLocation} from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
 
 function App() {
   return (
-    <>App</>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/signup' component={Signup}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
